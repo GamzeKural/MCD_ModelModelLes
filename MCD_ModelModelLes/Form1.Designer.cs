9a702c1a8835648e5060b9f2c46caf2f551e8b3d
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-08 20:54:19 +0300

    Add project files.

---
+++ MCD_ModelModelLes/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace MCD_ModelModelLes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGoster = new System.Windows.Forms.Button();
+            this.btnGosterPopup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGoster
+            // 
+            this.btnGoster.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGoster.Location = new System.Drawing.Point(492, 222);
+            this.btnGoster.Name = "btnGoster";
+            this.btnGoster.Size = new System.Drawing.Size(184, 54);
+            this.btnGoster.TabIndex = 0;
+            this.btnGoster.Text = "Göster";
+            this.btnGoster.UseVisualStyleBackColor = true;
+            this.btnGoster.Click += new System.EventHandler(this.btnGoster_Click);
+            // 
+            // btnGosterPopup
+            // 
+            this.btnGosterPopup.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGosterPopup.Location = new System.Drawing.Point(492, 313);
+            this.btnGosterPopup.Name = "btnGosterPopup";
+            this.btnGosterPopup.Size = new System.Drawing.Size(184, 54);
+            this.btnGosterPopup.TabIndex = 1;
+            this.btnGosterPopup.Text = "Göster Popup";
+            this.btnGosterPopup.UseVisualStyleBackColor = true;
+            this.btnGosterPopup.Click += new System.EventHandler(this.btnGosterPopup_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 689);
+            this.Controls.Add(this.btnGosterPopup);
+            this.Controls.Add(this.btnGoster);
+            this.Name = "Form1";
+            this.Text = "AnaForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGoster;
+        private System.Windows.Forms.Button btnGosterPopup;
+    }
+}
+
